52f21024fe6e8147a18385de13de668f91231553
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-02-01 15:46:11 +0530

    init

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+import tensorflow_probability as tfp  # We are using a different module from tensorflow this time
+import tensorflow as tf
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+tfd = tfp.distributions
+#The first day in our sequence has an 30% chance of being cold
+initial_distribution = tfd.Categorical(probs=[0.3, 0.7])
+#A cold day has a 50% chance of being followed by a hot day.
+#A hot day has a 20% chance of being followed by a cold day
+transition_distribution = tfd.Categorical(probs=[[0.5, 0.5],
+                                                 [0.2, 0.8]])
+#On each day the temperature is normally distributed with mean and standard deviation 0 and 5 on a cold day and mean and standard deviation 15 and 10 on a hot day.
+observation_distribution = tfd.Normal(loc=[0., 15.], scale=[5., 10.])
+
+model = tfd.HiddenMarkovModel(
+    initial_distribution=initial_distribution,
+    transition_distribution=transition_distribution,
+    observation_distribution=observation_distribution,
+    num_steps=7)
+
+mean = model.mean()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+with tf.compat.v1.Session() as sess:
+  print(mean.numpy())
